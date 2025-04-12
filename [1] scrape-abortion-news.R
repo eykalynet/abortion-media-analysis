50553f0f1242546963978d816fdb9d2510318481
Erika Lynet Salvador <168515072+eykalynet@users.noreply.github.com>
2025-04-11 21:24:48 -0400

    update scraping permissions and load SerpApi key

--- [1] scrape-abortion-news.R
+++ [1] scrape-abortion-news.R
@@ -1,4 +1,31 @@
+# ==============================================================================
+# Purpose: Scrape and extract data from the White House Presidential Actions page
+# Main Author: Erika Salvador
+# Contributors: Maigan Lafontant, Emilie Ward
+# Date modified: 2025-03-25
+# ==============================================================================
+
+# Load libraries
 library(robotstxt)
+library(rvest)
+library(dplyr)
+library(stringr)
+library(lubridate)
+library(purrr)
+
+# ==============================================================================
+# Check scraping permissions
+# ==============================================================================
 
 paths_allowed("https://www.foxnews.com/?msockid=1f6d91ae1b076b4d3b0d85791a9a6afe")
 paths_allowed("https://www.msnbc.com/")
+
+# ==============================================================================
+# Load SerpApi key securely
+# ==============================================================================
+
+api_key <- Sys.getenv("SERPAPI_KEY")
+if (api_key == "") stop("Missing API key! Please set SERPAPI_KEY in your .Renviron file.")
+
+  # Check README.md for set-up instructions
+
